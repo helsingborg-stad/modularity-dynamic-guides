4f323e4cd626f9d7667ac32225bdc3fe7fff7061
GitHub Action <>
2024-03-05 08:51:17 +0000

    build: update package file versions to 1.4.7

--- modularity-dynamic-guides.php
+++ modularity-dynamic-guides.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Dynamic Guides
  * Plugin URI:        https://github.com/NiclasNorin/modularity-dynamic-guides
  * Description:       A plugin to create dynamic guides
- * Version: 1.4.6
+ * Version: 1.4.7
  * Author:            Niclas Norin
  * Author URI:        https://github.com/NiclasNorin
  * License:           MIT
